90fdf4f1b90afee449c9f9f39e2e58c8ca038b58
Tomasz Rybak <tomasz.rybak@travelist.pl>
2025-07-01 14:12:49 +0200

    Add sample sentry DNS

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "...",
+  dsn: "https://e23dd06d7bff44f18d86f33878e67891@o19635.ingest.sentry.io/6173453",
   sendDefaultPii: true,
   _experiments: { enableLogs: true },
   tracesSampleRate: 1.0,
